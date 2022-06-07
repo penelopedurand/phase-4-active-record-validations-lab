c4a2a4225f0341f5908e4d13a528ec9aa4fc4f89
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-06-07 19:42:30 -0400

    commit tree e42defa6957056853020bb7a163f3031c3469560

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :must_be_clickbait
+
+    
+
+    def must_be_clickbait
+        unless title.present?&&title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "Title must include clickbait-y words")
+        end
+    end
+
 end
